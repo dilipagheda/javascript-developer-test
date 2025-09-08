808e63a13662b2bc88e4cbc817fd3cdb4ecf5338
Dilip Agheda <dilip.agheda@bingoindustries.com>
2025-09-08 16:26:06 +1000

    Implemented requirements by completing a function called  get-arnie-quotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,18 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+async function processUrl(url) {
+  const response = await httpGet(url);
+  const body = JSON.parse(response.body);
+  if (response.status === 200) {
+    return { "Arnie Quote": body.message };
+  } else {
+    return { FAILURE: body.message };
+  }
+}
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const requests = urls.map(processUrl);
+  return Promise.all(requests);
 };
 
 module.exports = {
